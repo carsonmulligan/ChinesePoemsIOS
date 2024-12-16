213675d5a2c10f6873a2f12d8145a3286ff48c1e
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-15 21:24:35 -0500

    Initial Commit

---
+++ ChinesePoems/ChinesePoemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChinesePoemsApp.swift
+//  ChinesePoems
+//
+//  Created by Carson Mulligan on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ChinesePoemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
