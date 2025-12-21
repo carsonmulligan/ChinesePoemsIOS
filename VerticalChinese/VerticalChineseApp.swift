433d84be502308dd4ba8b1bd806d3a2ea45ac079
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-12-21 09:05:39 -0600

    Add VerticalChinese target and associated resources

---
+++ VerticalChinese/VerticalChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalChineseApp.swift
+//  VerticalChinese
+//
+//  Created by Carson Mulligan on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct VerticalChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
